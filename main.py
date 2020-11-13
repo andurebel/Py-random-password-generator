161e38ce33bb2afc0bca6bd86422c0e263d0b7dc
Andu <andu.trica@gmail.com>
2020-11-13 13:55:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+
+uppercase_letter="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letter=uppercase_letter.lower()
+digits='0123456789'
+
+symbols= "!@#$%^&*()/ ?"
+upper,lower,nums,syms=True,True,True,True
+
+all=""
+if upper:
+  all+=uppercase_letter
+if lower:
+  all+=lowercase_letter
+if nums:
+  all+=digits
+if syms:
+  all+=symbols
+
+length=20
+amount=10
+for x in range(amount):
+  password="".join(random.sample(all,length))
+  print(password)
+  
